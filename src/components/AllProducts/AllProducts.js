7e23af57d5cd84fae86ef3e5c34bf75245e1ccb9
tomas <polonskytomas@gmail.com>
2024-06-12 18:50:59 -0300

    cards de productos, all products, pagination, paleta de colores

--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,98 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './allproducts.css'
 import ProductCard from '../Product/ProductCard';
+import axios from 'axios';
 
 const AllProducts = () => {
-  const products = [
-    {
-      id: 1,
-      name: 'Producto 1',
-      price: 100,
-      oldPrice: 150,
-      image1: 'https://acdn.mitiendanube.com/stores/001/490/877/products/trapstar-cyan-atras-fp-1-376f21a478c098065217024143772360-1024-1024.webp',
-      image2: 'https://acdn.mitiendanube.com/stores/001/490/877/products/img_0777-99b1309c1cac607ac916981007619856-640-0-d1a8d6435bf760f7fd17007609153890-640-0.webp',
-      stock: 20,
-    },{
-      id: 2,
-      name: 'Producto 2',
-      price: 1212,
-      oldPrice: 1500,
-      image1: 'https://minecraft.wiki/images/Enchanted_Golden_Apple_JE2_BE2.gif?2254a',
-      image2: 'https://i.pinimg.com/736x/61/a3/f7/61a3f7f2d6b126a6920f070756b2b147.jpg',
-      stock: 20,
-    },
-    {
-      id: 2,
-      name: 'Producto 2',
-      price: 1212,
-      oldPrice: 1500,
-      image1: 'https://minecraft.wiki/images/Enchanted_Golden_Apple_JE2_BE2.gif?2254a',
-      image2: 'https://i.pinimg.com/736x/61/a3/f7/61a3f7f2d6b126a6920f070756b2b147.jpg',
-      stock: 20,
-    },
-    {
-      id: 2,
-      name: 'Producto 2',
-      price: 1212,
-      oldPrice: 1500,
-      image1: 'https://minecraft.wiki/images/Enchanted_Golden_Apple_JE2_BE2.gif?2254a',
-      image2: 'https://i.pinimg.com/736x/61/a3/f7/61a3f7f2d6b126a6920f070756b2b147.jpg',
-      stock: 20,
-    },
-    {
-      id: 2,
-      name: 'Producto 2',
-      price: 1212,
-      oldPrice: 1500,
-      image1: 'https://minecraft.wiki/images/Enchanted_Golden_Apple_JE2_BE2.gif?2254a',
-      image2: 'https://i.pinimg.com/736x/61/a3/f7/61a3f7f2d6b126a6920f070756b2b147.jpg',
-      stock: 20,
-    },
-    {
-      id: 1,
-      name: 'Producto 1',
-      price: 100,
-      oldPrice: 150,
-      image1: 'https://acdn.mitiendanube.com/stores/001/490/877/products/trapstar-cyan-atras-fp-1-376f21a478c098065217024143772360-1024-1024.webp',
-      image2: 'https://acdn.mitiendanube.com/stores/001/490/877/products/img_0777-99b1309c1cac607ac916981007619856-640-0-d1a8d6435bf760f7fd17007609153890-640-0.webp',
-      stock: 20,
-    }
-    ,
-    {
-      id: 1,
-      name: 'Producto 1',
-      price: 100,
-      oldPrice: 150,
-      image1: 'https://acdn.mitiendanube.com/stores/001/490/877/products/trapstar-cyan-atras-fp-1-376f21a478c098065217024143772360-1024-1024.webp',
-      image2: 'https://acdn.mitiendanube.com/stores/001/490/877/products/img_0777-99b1309c1cac607ac916981007619856-640-0-d1a8d6435bf760f7fd17007609153890-640-0.webp',
-      stock: 20,
-    },
-    {
-      id: 1,
-      name: 'Producto 1',
-      price: 100,
-      oldPrice: 150,
-      image1: 'https://acdn.mitiendanube.com/stores/001/490/877/products/trapstar-cyan-atras-fp-1-376f21a478c098065217024143772360-1024-1024.webp',
-      image2: 'https://acdn.mitiendanube.com/stores/001/490/877/products/img_0777-99b1309c1cac607ac916981007619856-640-0-d1a8d6435bf760f7fd17007609153890-640-0.webp',
-      stock: 20,
-    }
-    // Agrega más productos aquí...
-  ];
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('http://192.168.0.119:4000/api/getProducts');
+        setProducts(response.data.AllProducts);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    getProducts();
+  }, []);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Logic for displaying products on the current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Logic for page numbers
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(products.length / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
   return (
-  
-      <div className="products-container">
+    <div className="products-container">
       <div className="products-header">
         <span className="category">Categoría</span>
         <span className="filter">Filtrar</span>
       </div>
       <div className="products-grid">
-        {products.map((product) => (
+        {currentProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <div className="pagination">
+        {pageNumbers.map(number => (
+          <button
+            key={number}
+            className={`page-number ${currentPage === number ? 'active' : ''}`}
+            onClick={() => handlePageChange(number)}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
     </div>
-      
   )
 }
 
-export default AllProducts
+export default AllProducts;
--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -4,21 +4,23 @@ import './ProductCard.css';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="product-card">
-      <img src={product.image1} alt={product.name} className="product-image primary1" />
-      <img src={product.image2} alt={product.name} className="product-image secondary1" />
-      <div className="product-info">
-        <span className="product-name">{product.name}</span>
-        <div className="product-pricing">
-          <span className="product-price">${product.price}</span>
-          <span className="product-old-price">${product.oldPrice}</span>
-        </div>
-      </div>
-      <div className="product-hover-info">
-        <span className="product-stock">Stock: {product.stock}</span>
-        <button className="add-to-cart-button">Agregar al Carrito</button>
-      </div>
+        <div className="product-card">
+          
+          <img src={product.imagen} alt={product.nombre} className="product-image primary1" />
+          <img src={product.imagen2} alt={product.nombre} className="product-image secondary1" />
+          <div className="product-info">
+            
+            <span className="product-name">{product.nombre}</span>
+            <div className="product-pricing">
+              <span className="product-price">${product.precio}</span>
+              <span className="product-old-price">${product.oldPrice}</span>
+            </div>
+          </div>  
+          <div className="product-hover-info">
+      <button className="add-to-cart-button">Agregar al Carrito</button>
     </div>
+      </div>
+
   );
 };
 
